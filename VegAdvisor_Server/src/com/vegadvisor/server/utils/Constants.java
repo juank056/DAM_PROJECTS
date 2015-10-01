276c4d1ccd522f5429cd0d5ebb1ff58594cfefd4
juank056 <juank_056@hotmail.com>
2015-10-01 14:30:41 +0200

    Second commit

---
+++ VegAdvisor_Server/src/com/vegadvisor/server/utils/Constants.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.vegadvisor.server.utils;
+
+/**
+ * Clase para manejar las constantes de la aplicacion
+ * 
+ * @author JuanCamilo
+ *
+ */
+public class Constants {
+
+}
