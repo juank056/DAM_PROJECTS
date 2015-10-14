40cae37bcc1f5f237637bb28b163913e42d35648
juank056 <juank_056@hotmail.com>
2015-10-14 12:38:37 +0200

    Vegadvisor services package creation

---
+++ VegAdvisor_Server/src/com/vegadvisor/server/services/rest/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * En este paquete se encontrarán la definición de los servicios que implementa la aplicación
+ */
+/**
+ * @author JuanCamilo
+ *
+ */
+package com.vegadvisor.server.services.rest;
