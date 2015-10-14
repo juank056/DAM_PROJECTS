8e17f8ebbfbc8ca779e36f15e7459a182eda14b5
juank056 <juank_056@hotmail.com>
2015-10-14 12:12:41 +0200

    Commit dbsupport and basics

---
+++ VegAdvisor_Server/src/com/vegadvisor/server/utils/ConfigApplicationManager.java
@@ -0,0 +1,89 @@
+package com.vegadvisor.server.utils;
+
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.apache.commons.lang.exception.ExceptionUtils;
+
+import com.vegadvisor.server.utils.exception.MissingConfigurationParameterException;
+
+/**
+ * Clase para manejar los parametros del archivo de configuracion
+ * 
+ * @author ESFDES27
+ *
+ */
+public final class ConfigApplicationManager {
+
+	/**
+	 * Indica la ruta donde se encuentra el archivo de configuracion de la
+	 * aplicacion unica ruta en codigo "quemada".
+	 */
+	private static final String CONFIGURATION_FILE = "../conf/config.properties";
+	private static Map<String, String> parameters;
+	static {
+		reloadParameters();
+	} // static
+
+	/**
+	 * Constructor privado
+	 */
+	private ConfigApplicationManager() {
+	}
+
+	/**
+	 * Metodo para obtener el valor de un parametro
+	 * 
+	 * @param name
+	 *            el nombre del parametro a obtener
+	 * @return el valor del parametro.
+	 * @throws MissingConfigurationParameterException
+	 *             en caso de no encontrar el parametro
+	 */
+	public static String getParameter(String name)
+			throws MissingConfigurationParameterException {
+		String value = parameters.get(name);
+		if (value == null) {
+			throw new MissingConfigurationParameterException(
+					"Parameter Not Found: " + name);
+		}
+		return value;
+	}
+
+	/**
+	 * Regarga los parametros del archivo de configuracion
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public static void reloadParameters() {
+		try {
+			/* Read property file (if exists). */
+			Properties properties = new Properties();
+			InputStream inputStream = null;
+			try {
+				Class configParametersManagerClass = ConfigApplicationManager.class;
+				ClassLoader classLoader = configParametersManagerClass
+						.getClassLoader();
+				inputStream = classLoader
+						.getResourceAsStream(CONFIGURATION_FILE);
+			} catch (Exception e) {
+				System.out
+						.println("Input stream null. The application will now terminate.\nStack Trace:\n");
+				System.out.println(ExceptionUtils.getFullStackTrace(e));
+				System.exit(-1);
+			}
+			properties.load(inputStream);
+			inputStream.close();
+			/*
+			 * We use a "HashMap" instead of a "HashTable" because HashMap's
+			 * methods are *not* synchronized (so they are faster), and the
+			 * parameters are only read.
+			 */
+			parameters = new HashMap(properties);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+} // class
+
--- VegAdvisor_Server/src/com/vegadvisor/server/utils/Constants.java
+++ VegAdvisor_Server/src/com/vegadvisor/server/utils/Constants.java
@@ -3,6 +3,8 @@
  */
 package com.vegadvisor.server.utils;
 
+import java.util.Date;
+
 /**
  * Clase para manejar las constantes de la aplicacion
  * 
@@ -12,23 +14,168 @@ package com.vegadvisor.server.utils;
 public class Constants {
 
 	/**
-	 * Blancos
+	 * Fecha por default
+	 */
+	@SuppressWarnings("deprecation")
+	public static final Date DEFAULT_DATE = new java.util.Date(70, 0, 1);
+
+	/**
+	 * Cadena en blancos
 	 */
 	public static final String BLANKS = "";
 
 	/**
-	 * Espacio en blancos
+	 * Punto
 	 */
-	public static final String BLANK_SPACE = " ";
-	
+	public static final String POINT = ".";
+
 	/**
-	 * Slash 
+	 * Coma
+	 */
+	public static final String COMMA = ",";
+
+	/**
+	 * Punto y coma
+	 */
+	public static final String COMMA_DOT = ";";
+
+	/**
+	 * Cero
+	 */
+	public static final String ZERO = "0";
+
+	/**
+	 * Slash
 	 */
 	public static final String SLASH = "/";
-	
+
 	/**
-	 * Plus 
+	 * Linea del piso
 	 */
-	public static final String PLUS = "+";
+	public static final String UNDER_SCORE = "_";
+
+	/**
+	 * Uno
+	 */
+	public static final String ONE = "1";
+
+	/**
+	 * Tiempo estandar para dormir y esperar
+	 */
+	public static final long SLEEP_TIME = 15000;
+
+	/**
+	 * True en Char lince
+	 */
+	public static final char TRUE = '1';
+
+	/**
+	 * Menos
+	 */
+	public static final String MINUS = "-";
+	/**
+	 * Menos Cero
+	 */
+	public static final String MINUS_ZERO = "-0";
+
+	/**
+	 * Backup
+	 */
+	public static final String BK = "BK";
+
+	/**
+	 * Recover
+	 */
+	public static final String RC = "RC";
+
+	/**
+	 * Sql Extension
+	 */
+	public static final String SQL_EXT = ".sql";
+
+	/**
+	 * Parametro de Url 1
+	 */
+	public static final String URL_PARAM01 = "&1";
+
+	/**
+	 * f
+	 */
+	public static final String F = "F";
+
+	/**
+	 * Smtp
+	 */
+	public static final String SMTP = "smtp";
+
+	public static final String TWO_POINTS = ":";
+
+	public static final String TWO_POINTS_ZERO = ":0";
+
+	public static final String THREE = "3";
+
+	public static final String TWO = "2";
+
+	/**
+	 * Separador decimal para split
+	 */
+	public static final String DECIM_SEP = "\\.";
+
+	/**
+	 * Punto decimal
+	 */
+	public static final String DEC_POINT = ".";
+
+	public static final String BLANK_SPACE = " ";
+
+	/**
+	 * Cinco
+	 */
+	public static final String FIVE = "5";
+
+	/**
+	 * Cuatro
+	 */
+	public static final String FOUR = "4";
+
+	/**
+	 * Ceros
+	 */
+	public static final String ZEROS = "00";
+
+	/**
+	 * Parametro 01
+	 */
+	public static final String PARAM01 = "01";
+
+	/**
+	 * Parametro 02
+	 */
+	public static final String PARAM02 = "02";
+
+	/**
+	 * Parametro 03
+	 */
+	public static final String PARAM03 = "03";
+
+	/**
+	 * Separador interno de parametros
+	 */
+	public static final String INNER_PARAM_SEP = "_";
+
+	/**
+	 * Flecha
+	 */
+	public static final String ARROW = "==>";
+
+	/**
+	 * Parentesis izquierdo
+	 */
+	public static final String LEFT_PARENTHESIS = "[";
+
+	/**
+	 * Parentesis derecho
+	 */
+	public static final String RIGHT_PARENTHESIS = "]";
 
 }
