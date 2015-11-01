0ffca24d1e76cb40cf230ec2702ca27b79141265
juank056 <juank_056@hotmail.com>
2015-11-01 10:12:26 +0100

    Test Framework Creation

--- VegAdvisor_Server/src/com/vegadvisor/server/services/rest/UserServicesREST.java
+++ VegAdvisor_Server/src/com/vegadvisor/server/services/rest/UserServicesREST.java
@@ -12,6 +12,7 @@ import javax.ws.rs.core.MediaType;
 
 import com.vegadvisor.server.services.IUserServices;
 import com.vegadvisor.server.services.bo.ReturnValidation;
+import com.vegadvisor.server.utils.DateUtils;
 import com.vegadvisor.server.utils.LogLogger;
 import com.vegadvisor.server.utils.SpringAppContext;
 
@@ -95,6 +96,80 @@ public class UserServicesREST {
 		ReturnValidation response = userServices.createUser(userId, userName,
 				userLastName, email, password);
 		LogLogger.getInstance(getClass()).logger("Finaliza createUser",
+				LogLogger.DEBUG);
+		// Retornamos
+		return response;
+	}
+
+	/**
+	 * Actualiza usuario del sistema
+	 * 
+	 * @param userId
+	 *            Id del usuario a actualizar
+	 * @param userName
+	 *            nombre del usuario
+	 * @param userLastName
+	 *            apellido del usuarii
+	 * @param email
+	 *            Email del usuario
+	 * @param password
+	 *            contraseña del usuario
+	 * @param dateOfBirth
+	 *            Fecha de nacimiento (Formato YYYYMMDD)
+	 * @param countryCode
+	 *            Código de pais
+	 * @param cityCode
+	 *            Código de ciudad
+	 * @param isVegan
+	 *            Indicador de vegano
+	 * @param hobbies
+	 *            Hobbies
+	 * @param gender
+	 *            Género
+	 * @return Retorno de validación
+	 */
+	@POST
+	@Path("/updateUser")
+	@Consumes({ MediaType.APPLICATION_FORM_URLENCODED,
+			MediaType.APPLICATION_JSON })
+	@Produces(MediaType.APPLICATION_JSON)
+	public ReturnValidation updateUser(@FormParam("userId") String userId,
+			@FormParam("userName") String userName,
+			@FormParam("userLastName") String userLastName,
+			@FormParam("email") String email,
+			@FormParam("password") String password,
+			@FormParam("dateOfBirth") String dateOfBirth,
+			@FormParam("countryCode") String countryCode,
+			@FormParam("cityCode") String cityCode,
+			@FormParam("isVegan") String isVegan,
+			@FormParam("hobbies") String hobbies,
+			@FormParam("gender") String gender) {
+		LogLogger.getInstance(getClass()).logger("Inicia updateUser",
+				LogLogger.DEBUG);
+		// Actualizacion usuario
+		ReturnValidation response = userServices.updateUser(userId, userName,
+				userLastName, email, password,
+				DateUtils.getDateDateYYYYMMDD(dateOfBirth), countryCode,
+				cityCode, isVegan, hobbies, gender);
+		LogLogger.getInstance(getClass()).logger("Finaliza updateUser",
+				LogLogger.DEBUG);
+		// Retornamos
+		return response;
+	}
+
+	@POST
+	@Path("/checkInUser")
+	@Consumes({ MediaType.APPLICATION_FORM_URLENCODED,
+			MediaType.APPLICATION_JSON })
+	@Produces(MediaType.APPLICATION_JSON)
+	public ReturnValidation checkInUser(@FormParam("userId") String userId,
+			@FormParam("establishmentId") int establishmentId) {
+		LogLogger.getInstance(getClass()).logger("Inicia checkInUser",
+				LogLogger.DEBUG);
+		// Checkin de uuario en establecimiento
+		ReturnValidation response = userServices.checkInUser(userId,
+				establishmentId);
+		LogLogger.getInstance(getClass()).logger("Finaliza checkInUser",
 				LogLogger.DEBUG);
 		// Retornamos
 		return response;
